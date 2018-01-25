e0c786d7e5cbcf9abbb23bc8619cc8fda37fea28
Xristian93 <cristianfuente72@gmail.com>
2018-01-25 15:47:29 +0100

    Añadida clase test

---
+++ UnitTests.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UnitTests {
+  public void test01() {
+    LigaDeFutbol LigaDeFutbol1 = new LigaDeFutbol();
+    System.out.println("Añadiendo 6 equipos a la coleccion...");
+    System.out.println(" ");
+    LigaDeFutbol1.añadirEquipo("Liverpool");
+    LigaDeFutbol1.añadirEquipo("Manchester United");
+    LigaDeFutbol1.añadirEquipo("Manchester City");
+    LigaDeFutbol1.añadirEquipo("Chelsea");
+    LigaDeFutbol1.añadirEquipo("Arsenal");
+    LigaDeFutbol1.añadirEquipo("Everton");
+    //Cambiando numero de victorias y derrotas de los equipos
+    LigaDeFutbol1.cambiarVictorias(0,10);   LigaDeFutbol1.cambiarDerrotas(0,1);
+    LigaDeFutbol1.cambiarVictorias(1,4);    LigaDeFutbol1.cambiarDerrotas(1,10);
+    LigaDeFutbol1.cambiarVictorias(2,9);    LigaDeFutbol1.cambiarDerrotas(2,3);
+    LigaDeFutbol1.cambiarVictorias(3,4);    LigaDeFutbol1.cambiarDerrotas(3,8);
+    LigaDeFutbol1.cambiarVictorias(4,8);    LigaDeFutbol1.cambiarDerrotas(4,5);
+    LigaDeFutbol1.cambiarVictorias(5,12);   LigaDeFutbol1.cambiarDerrotas(5,2);
+    System.out.println("Mostrando por pantalla los datos de los objetos añadidos...");
+    System.out.println(" ");
+    LigaDeFutbol1.listEquipos();
+    System.out.println(" ");
+    System.out.println("Mostrando ordenadamente por numero de victorias...");
+    System.out.println(" ");
+    LigaDeFutbol1.ordenarPorVictorias();
+    System.out.println(" ");
+    System.out.println("Mostrando ordenadamente por numero de derrotas...");
+    System.out.println(" ");
+    LigaDeFutbol1.ordenarPorDerrotas();
+    System.out.println(" ");
+    System.out.println("Modificando las caracteristicas de un par de objetos...");
+    System.out.println(" ");
+    LigaDeFutbol1.cambiarVictorias(4,11);
+    LigaDeFutbol1.cambiarDerrotas(1,12);
+    System.out.println("Eliminando los equipos con menos de 10 victorias...");
+    System.out.println(" ");
+    LigaDeFutbol1.eliminarPorVictorias(10);
+    System.out.println("Mostrando de nuevo la lista de objetos...");
+    System.out.println(" ");
+    LigaDeFutbol1.listEquipos();
+  }
+}
